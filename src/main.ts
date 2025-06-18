89a487be9a2668fe3a6c1ba9038729c3d8c65cb3
AlexandraLimaG <agdelima321@gmail.com>
2025-06-18 17:20:40 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  process.env.TZ = '-03:00'
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  app.enableCors({
+    origin: 'https://blogale.com', // Substitua pelo domínio permitido
+    methods: 'GET,POST,PUT,DELETE', // Métodos permitidos
+    allowedHeaders: 'Content-Type, Authorization', // Cabeçalhos permitidos
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
